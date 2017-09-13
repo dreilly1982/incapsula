a737e8bca833a63018820281e868868abf07a76b
Reilly, Don <don.reilly@ercot.com>
2017-09-13 12:45:37 -0500

    remove support for 2.6

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         'Topic :: Internet',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
